aac900fd68693b2aa7161b8650d540afca09a638
emrecem <emrecem.kaya@gmail.com>
2020-11-22 21:31:01 +0100

    Added more time before timeout

--- friendTroller.py
+++ friendTroller.py
@@ -13,9 +13,9 @@ url = 'https://web.whatsapp.com/'
 browser.get(url)
 ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
 #Add Contact name or Group name below
-WebDriverWait(browser, 10,ignored_exceptions=ignored_exceptions).until(expected_conditions.presence_of_element_located((By.XPATH, "//span[@title='FRIEND NAME HERE']")))
-keremChat = browser.find_element_by_xpath("//span[@title='Kerem Oner']")
-keremChat.click()
+WebDriverWait(browser, 60,ignored_exceptions=ignored_exceptions).until(expected_conditions.presence_of_element_located((By.XPATH, "//span[@title='FRIEND NAME HERE']")))
+Chat = browser.find_element_by_xpath("//span[@title='FRIEND NAME HERE']")
+Chat.click()
 chatTextBox = browser.find_element(By.CSS_SELECTOR, ".DuUXI")
 chatText = chatTextBox.find_element(By.CSS_SELECTOR, "._1awRl.copyable-text.selectable-text")
 for i in range(3600):
